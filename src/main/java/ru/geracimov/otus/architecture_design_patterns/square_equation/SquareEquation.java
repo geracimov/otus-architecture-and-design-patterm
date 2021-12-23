62fbba6c488cdcbb6a3e5b55ef3521488832c5e5
geracimov <geracimov@gmail.com>
2021-12-24 00:54:16 +0300

    Написал тест, который проверяет, что для уравнения x^2-1 = 0 есть два корня кратности 1 (x1=1, x2=-1)

--- src/main/java/ru/geracimov/otus/architecture_design_patterns/square_equation/SquareEquation.java
+++ src/main/java/ru/geracimov/otus/architecture_design_patterns/square_equation/SquareEquation.java
@@ -2,6 +2,10 @@ package ru.geracimov.otus.architecture_design_patterns.square_equation;
 
 public class SquareEquation {
     public double[] solve(double a, double b, double c) {
+        double d = b * b - 4 * a * c;
+        if (Double.compare(d, 0.0) > 0) {
+            return new double[]{(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)};
+        }
         return new double[0];
     }
 }
--- src/test/java/ru/geracimov/otus/architecture_design_patterns/square_equation/SquareEquationTest.java
+++ src/test/java/ru/geracimov/otus/architecture_design_patterns/square_equation/SquareEquationTest.java
@@ -26,4 +26,10 @@ class SquareEquationTest {
         assertArrayEquals(new double[]{}, squareEquation.solve(1.0, 0.0, 1.0));
     }
 
+    @Test
+    @DisplayName("тест, который проверяет, что для уравнения x^2-1 = 0 есть два корня кратности 1 (x1=1, x2=-1)")
+    void quadraticEquationHasTwoRootsOfMultiplicity1Test() {
+        assertArrayEquals(new double[]{1.0, -1.0}, squareEquation.solve(1.0, 0.0, -1.0));
+    }
+
 }
