b55b10b85d8e7ee7f0a015154bda83c04a7ab73c
geracimov <geracimov@gmail.com>
2021-12-24 00:59:26 +0300

    Написал тест, который проверяет, что для уравнения x^2+2x+1 = 0 есть один корень кратности 2 (x1= x2 = -1)

--- src/test/java/ru/geracimov/otus/architecture_design_patterns/square_equation/SquareEquationTest.java
+++ src/test/java/ru/geracimov/otus/architecture_design_patterns/square_equation/SquareEquationTest.java
@@ -23,13 +23,22 @@ class SquareEquationTest {
     @Test
     @DisplayName("тест, который проверяет, что для уравнения x^2+1 = 0 корней нет (возвращается пустой массив)")
     void squareEquationHasNoRootsTest() {
-        assertArrayEquals(new double[]{}, squareEquation.solve(1.0, 0.0, 1.0));
+        double[] roots = squareEquation.solve(1.0, 0.0, 1.0);
+        assertArrayEquals(new double[]{}, roots);
     }
 
     @Test
     @DisplayName("тест, который проверяет, что для уравнения x^2-1 = 0 есть два корня кратности 1 (x1=1, x2=-1)")
     void quadraticEquationHasTwoRootsOfMultiplicity1Test() {
-        assertArrayEquals(new double[]{1.0, -1.0}, squareEquation.solve(1.0, 0.0, -1.0));
+        double[] roots = squareEquation.solve(1.0, 0.0, -1.0);
+        assertArrayEquals(new double[]{1.0, -1.0}, roots);
+    }
+
+    @Test
+    @DisplayName("тест, который проверяет, что для уравнения x^2+2x+1 = 0 есть один корень кратности 2 (x1= x2 = -1)")
+    void quadraticEquationHasOneRootsOfMultiplicity2Test() {
+        double[] roots = squareEquation.solve(1.0, 2.0, 1.0);
+        assertArrayEquals(new double[]{-1.0, -1.0}, roots);
     }
 
 }
